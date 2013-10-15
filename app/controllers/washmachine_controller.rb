a5619d1bb1959acb2f1e88e99eb5f19ac2be46a2
Jan van der Pas <janvanderpas@gmail.com>
2013-10-15 22:35:32 +0200

    Prepare API for washmachine

--- app/controllers/washmachine_controller.rb
+++ app/controllers/washmachine_controller.rb
@@ -1,5 +1,27 @@
 class WashmachineController < ApplicationController
 
+  # def index
+  # end
+
+  respond_to :json
+
   def index
+    respond_with Washmachine.all
+  end
+
+  def show
+    respond_with Washmachine.find(params[:id])
+  end
+
+  def create
+    respond_with Washmachine.create(params[:washmachine])
+  end
+
+  def update
+    respond_with Washmachine.update(params[:id], params[:washmachine])
+  end
+
+  def destroy
+    respond_with Washmachine.destroy(params[:id])
   end
 end
